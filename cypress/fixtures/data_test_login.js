d414c84820d31c508687730dee82b554c2d35b8d
hanhsymper <hanhdth@symper.vn>
2022-06-17 15:51:59 +0700

    data test login

---
+++ cypress/fixtures/data_test_login.js
@@ -0,0 +1,38 @@
+module.exports = [
+  //   {
+  //     title: "login success",
+  //     email: "hanhdth@symper.vn",
+  //     password: "Symper@123BA",
+  //     expect: true,
+
+  //     messages: [],
+  //   },
+  {
+    title: "login email invalid",
+    email: "hanhdthsymper.vn",
+    password: "Symper@123BA",
+    expect: "check_validate",
+    messages: ["Email không hợp lệ"],
+  },
+  {
+    title: "password empty",
+    email: "hanhdth@symper.vn",
+    password: "    ",
+    expect: "check_validate",
+    messages: ["Mật khẩu không được để trống"],
+  },
+  {
+    title: "password empty",
+    email: "hanhdth@symper.vn",
+    password: "zsdfgh",
+    expect: "login_not_success",
+    messages: ["Không thể đăng nhậpTài khoản hoặc mật khẩu không chính xác!"],
+  },
+
+  {
+    title: "login success",
+    email: "hanhdth@symper.vn",
+    password: "Symper@123BA",
+    expect: "check_cuccess",
+  },
+];
