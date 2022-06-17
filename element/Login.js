0ad1c4972028b4ed36f5626a5eb36bc3c45da3d5
hanhsymper <hanhdth@symper.vn>
2022-06-17 15:52:53 +0700

    các element để test login

---
+++ element/Login.js
@@ -0,0 +1,5 @@
+export const txtEmail = 'input[name="login"]';
+export const txtPassword = 'input[name="password"]';
+export const btnLogin = "Đăng nhập";
+export const messages = ".v-messages__wrapper .v-messages__message";
+export const notification = ".vue-notification-wrapper";
