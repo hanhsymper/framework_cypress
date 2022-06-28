365eb935b7cc07e31fbf8b237eb9230ad39e6ca6
hanhsymper <hanhdth@symper.vn>
2022-06-28 08:31:01 +0700

    các element trong page application

---
+++ element/Application.mjs
@@ -0,0 +1,11 @@
+// export const btnApplication = "Ứng dụng của tôi";
+export const btnApplication =
+  ".v-icon.notranslate.collapse.icon-group.v-icon--link.mdi.mdi-briefcase-edit-outline.theme--light";
+
+export const openSilder =
+  ".list-item.v-list-item.v-list-item--two-line.theme--light.ma-0";
+export const tolltipRole =
+  ".v-btn.v-btn--depressed.v-btn--flat.v-btn--text.theme--light.v-size--x-small";
+export const selectRole =
+  ".v-menu__content.theme--light.v-menu__content--fixed.menuable__content__active .py-1.px-0.fs-16.pl-4.pr-2.grey-hover.position-relative";
+export const nameRole = "hanh";
