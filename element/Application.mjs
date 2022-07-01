702ecb0fe8362932aab9f16ce159555536c25221
hanhsymper <hanhdth@symper.vn>
2022-07-01 07:48:33 +0700

    element test app

--- element/Application.mjs
+++ element/Application.mjs
@@ -9,3 +9,16 @@ export const tolltipRole =
 export const selectRole =
   ".v-menu__content.theme--light.v-menu__content--fixed.menuable__content__active .py-1.px-0.fs-16.pl-4.pr-2.grey-hover.position-relative";
 export const nameRole = "hanh";
+export const openDetailApp = ".title-app-sbs";
+export const nameWFL = "HANH TEST <đừng xóa>";
+export const selectWFL = ".app-child-item li";
+
+export const selectActionWFL = ".context-menu-item";
+export const nameActionWFL = "Bắt đầu";
+
+export const btn_icon_opensidebar =
+  ".v-btn.v-btn--flat.v-btn--icon.v-btn--round.v-btn--tile.theme--light.v-size--default .v-icon.notranslate.mdi.mdi.mdi.mdi-chevron-right.theme--light";
+export const icon_config =
+  ".v-icon.notranslate.mdi.mdi-cog-outline.theme--light";
+export const btn_logout = ".v-list-item__title.fs-13.fm";
+export const name_logout = "Đăng xuất";
