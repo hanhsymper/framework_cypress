1f25728ed98b6c8fdd245000f4b3a169c8d8b978
hanhsymper <hanhdth@symper.vn>
2022-07-01 07:48:56 +0700

    element page myitem

---
+++ element/MyItem.mjs
@@ -0,0 +1,22 @@
+export const btnMyItem =
+  ".v-icon.notranslate.collapse.icon-group.v-icon--link.mdi.mdi-format-list-checkbox.theme--light";
+
+export const textTitle = ".text-left.fs-13.text-ellipsis.w-100";
+export const input_start_date = "#s-control-id-1656298089201";
+export const input_text = "#s-control-id-1656298470969";
+export const input_stt = "#s-control-id-1656298154923";
+export const button_submit =
+  ".mr-1.white--text.v-btn.v-btn--depressed.theme--light.v-size--small.blue";
+export const button_complete =
+  ".v-btn.v-btn--contained.theme--light.v-size--small"; //ep0
+export const button_complete_success =
+  ".v-list-item.v-list-item--link.theme--light";
+export const btn_apply =
+  ".v-btn.v-btn--flat.v-btn--text.theme--light.v-size--default.green--text.text--darken-1";
+export const input_primary = "#s-control-id-1656297988119";
+export const btn_approval =
+  ".mr-1.white--text.v-btn.v-btn--depressed.theme--light.v-size--small.green";
+export const btn_disapproval =
+  ".mr-1.white--text.v-btn.v-btn--depressed.theme--light.v-size--small.red";
+export const btn_update =
+  ".mr-1.white--text.v-btn.v-btn--depressed.theme--light.v-size--small.yellow";
