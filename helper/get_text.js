b00596ebddfb5156ddc60f34d4009158099822ab
hanhsymper <hanhdth@symper.vn>
2022-07-01 07:49:34 +0700

    các hàm dùng chung

--- helper/get_text.js
+++ helper/get_text.js
@@ -1,4 +1,21 @@
+import { expect } from "chai";
+import { title } from "../cypress/fixtures/data_test_wfl/data_test_notification.mjs";
+
 export const gettitleListText = (data) => {
   let a = cy.get(data).then(($cells) => Cypress._.map($cells, "textContent"));
   return a;
+};
+// export const checkTitle = (data, title, i) => {
+//   cy.get(data).then(($cells) => {
+//     expect($cells.get(i).innerText).to.deep.equal(title);
+//   });
+export const checkTitle = (data, title, i) => {
+  cy.get(data)
+    .eq(i)
+    .invoke("text")
+    .then(cy.log)
+    .then((text) => {
+      return text.trim();
+    })
+    .should("equal", title);
 };
