ebf1a8e7fe3f66ed19e4a691f941d8423c14cf25
hanhsymper <hanhdth@symper.vn>
2022-07-05 07:06:57 +0700

    data test dataset

---
+++ cypress/fixtures/data_test_dataflow/data.js
@@ -0,0 +1,610 @@
+module.exports = [
+  {
+    dataset_name: "dataset_noi",
+    dataflow_id: 137,
+    id_node: ["c253f19a-498b-4756-a7cb-08fc958cb743"],
+  },
+  {
+    dataset_name: "lk_bcsxnl_th",
+    dataflow_id: 152,
+    id_node: ["54c35ab9-b1c2-4efe-9c4b-d10a5a78679a"],
+  },
+  {
+    dataset_name: "bc_tpcnk",
+    dataflow_id: 175,
+    id_node: ["c7793615-d292-4893-a6d2-1a9d4075f421"],
+  },
+  {
+    dataset_name: "bc_sx_thang_201113",
+    dataflow_id: 178,
+    id_node: ["6afdb215-b80f-4fb2-84d7-5e98bb924d12"],
+  },
+  {
+    dataset_name: "ds_thctsx",
+    dataflow_id: 193,
+    id_node: [
+      "944dea74-d0b5-437a-badc-5c5fbd9fdc44",
+      "811f330d-c973-4a42-88e7-1a4b9682207b",
+    ],
+  },
+  //   {
+  //     dataset_name: "ctsx_sgcm",
+  //     dataflow_id: 193,
+  //     id_node: "811f330d-c973-4a42-88e7-1a4b9682207b",
+  //   },
+  {
+    dataset_name: "data_lsx_v3",
+    dataflow_id: 199,
+    id_node: ["6126bbe7-30be-42a0-a117-4f4256131179"],
+  },
+  {
+    dataset_name: "te_vs_mh4",
+    dataflow_id: 200,
+    id_node: [
+      "7729d968-0fdd-4c5e-8511-5007af6f0694",
+      "548faf5a-a590-47a1-8895-b93905a164a7",
+      "8122b5a3-d74b-48eb-899d-c506aead795c",
+      "7729d968-0fdd-4c5e-8511-5007af6f0694",
+      "fcb686f0-a48c-417c-9d87-f282c26c2f2e",
+      "548faf5a-a590-47a1-8895-b93905a164a7",
+      "ec0dde30-1a26-49a6-8bd2-a7aaf1302732",
+      "9022cdfe-f755-44ac-b760-5ec9b2e548bc",
+      "cefbce19-399f-4a85-b020-6faee1dd943e",
+    ],
+  },
+  //   {
+  //     dataset_name: "te_vs_mh3",
+  //     dataflow_id: 200,
+  //     id_node: "548faf5a-a590-47a1-8895-b93905a164a7",
+  //   },
+  //   {
+  //     dataset_name: "e2e_pnk",
+  //     dataflow_id: 200,
+  //     id_node: "8122b5a3-d74b-48eb-899d-c506aead795c",
+  //   },
+  //   {
+  //     dataset_name: "vs_mh",
+  //     dataflow_id: 200,
+  //     id_node: "7729d968-0fdd-4c5e-8511-5007af6f0694",
+  //   },
+  //   {
+  //     dataset_name: "te_vs_mh1",
+  //     dataflow_id: 200,
+  //     id_node: "fcb686f0-a48c-417c-9d87-f282c26c2f2e",
+  //   },
+  //   {
+  //     dataset_name: "te_vs_3",
+  //     dataflow_id: 200,
+  //     id_node: "548faf5a-a590-47a1-8895-b93905a164a7",
+  //   },
+  //   {
+  //     dataset_name: "te_vs_mh6",
+  //     dataflow_id: 200,
+  //     id_node: "ec0dde30-1a26-49a6-8bd2-a7aaf1302732",
+  //   },
+  //   {
+  //     dataset_name: "te_vs_mh5",
+  //     dataflow_id: 200,
+  //     id_node: "9022cdfe-f755-44ac-b760-5ec9b2e548bc",
+  //   },
+  //   {
+  //     dataset_name: "te_vs_mh2",
+  //     dataflow_id: 200,
+  //     id_node: "cefbce19-399f-4a85-b020-6faee1dd943e",
+  //   },
+  {
+    dataset_name: "bcsx_nksx",
+    dataflow_id: 202,
+    id_node: [
+      "b5f829cc-7ae4-4ed9-93f3-bc178f04b1d8",
+      "96b64472-ab2f-45be-80a0-a7bab48e4626",
+    ],
+  },
+  //   {
+  //     dataset_name: "bcsx_hh",
+  //     dataflow_id: 202,
+  //     id_node: "96b64472-ab2f-45be-80a0-a7bab48e4626",
+  //   },
+  {
+    dataset_name: "th_kcs",
+    dataflow_id: 203,
+    id_node: [
+      "73e6acae-5440-437f-8120-668153f74938",
+      "100ba1a3-5b6e-4289-a9f5-edcd2de8282c",
+    ],
+  },
+  //   {
+  //     dataset_name: "thsx",
+  //     dataflow_id: 203,
+  //     id_node: "100ba1a3-5b6e-4289-a9f5-edcd2de8282c",
+  //   },
+  {
+    dataset_name: "khsx",
+    dataflow_id: 204,
+    id_node: [
+      "bb45906a-9a48-4d2a-be62-3bd4e677a1b7",
+      "732ed041-e106-45f2-bae6-e86542dac2d8",
+      "48bdd8fb-6773-420a-a46e-ea0a9391c828",
+    ],
+  },
+  //   {
+  //     dataset_name: "khsx",
+  //     dataflow_id: 204,
+  //     id_node: "732ed041-e106-45f2-bae6-e86542dac2d8",
+  //   },
+  //   {
+  //     dataset_name: "khsx_final",
+  //     dataflow_id: 204,
+  //     id_node: "48bdd8fb-6773-420a-a46e-ea0a9391c828",
+  //   },
+  {
+    dataset_name: "bcsx_bcsx2_than_nung",
+    dataflow_id: 207,
+    id_node: [
+      "e65b3505-427a-4764-bae2-e9a12741b7d5",
+      "f6fb4905-3689-4bd1-8364-d7874634e439",
+      "6ce09313-d4d9-4ead-b845-24939cc6ad53",
+      "35987217-4733-49cc-9d09-8dc25428547b",
+      "7a313c09-fbf5-474e-8ea0-3b603e9ba9b2",
+      "0a31c0de-4519-429f-83ac-ce4c403940ab",
+      "a8342be2-5582-49a8-9345-ce30e6a7b77f",
+      "f580e0e6-3747-4c6a-8b96-a3ac81843bdb",
+    ],
+  },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_e",
+  //     dataflow_id: 207,
+  //     id_node: "f6fb4905-3689-4bd1-8364-d7874634e439",
+  //   },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_g",
+  //     dataflow_id: 207,
+  //     id_node: "6ce09313-d4d9-4ead-b845-24939cc6ad53",
+  //   },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_men_tt",
+  //     dataflow_id: 207,
+  //     id_node: "35987217-4733-49cc-9d09-8dc25428547b",
+  //   },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_than_say",
+  //     dataflow_id: 207,
+  //     id_node: "7a313c09-fbf5-474e-8ea0-3b603e9ba9b2",
+  //   },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_xuong",
+  //     dataflow_id: 207,
+  //     id_node: "0a31c0de-4519-429f-83ac-ce4c403940ab",
+  //   },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_dieu",
+  //     dataflow_id: 207,
+  //     id_node: "a8342be2-5582-49a8-9345-ce30e6a7b77f",
+  //   },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_men_kh",
+  //     dataflow_id: 207,
+  //     id_node: "f580e0e6-3747-4c6a-8b96-a3ac81843bdb",
+  //   },
+  {
+    dataset_name: "hao_but_bot_ep",
+    dataflow_id: 208,
+    id_node: [
+      "12941617-e2fa-4f1b-a044-6d8266e9c4f7",
+      "f6b2fdf1-dfe1-4e91-b3db-ad8fe9fe4968",
+      "b84b7c24-ff9e-4011-b6d7-1ab8a291eeab",
+      "12941617-e2fa-4f1b-a044-6d8266e9c4f7",
+      "6e5f2497-b900-436a-98b1-8da9047f0185",
+      "a9c5f74a-db50-4fc9-b366-03ba0bc35358",
+      "40e9c97e-a1a8-43ad-ac91-7628382b4727",
+    ],
+  },
+  //   {
+  //     dataset_name: "th_bot_ep",
+  //     dataflow_id: 208,
+  //     id_node: "f6b2fdf1-dfe1-4e91-b3db-ad8fe9fe4968",
+  //   },
+  //   {
+  //     dataset_name: "tl_hh_bot_ep",
+  //     dataflow_id: 208,
+  //     id_node: "b84b7c24-ff9e-4011-b6d7-1ab8a291eeab",
+  //   },
+  //   {
+  //     dataset_name: "hao_hut_bot_ep",
+  //     dataflow_id: 208,
+  //     id_node: "12941617-e2fa-4f1b-a044-6d8266e9c4f7",
+  //   },
+  //   {
+  //     dataset_name: "th_ngl",
+  //     dataflow_id: 208,
+  //     id_node: "6e5f2497-b900-436a-98b1-8da9047f0185",
+  //   },
+  //   {
+  //     dataset_name: "tl_hh_bot_lieu",
+  //     dataflow_id: 208,
+  //     id_node: "a9c5f74a-db50-4fc9-b366-03ba0bc35358",
+  //   },
+  //   {
+  //     dataset_name: "bang_sl_th_bot_ep",
+  //     dataflow_id: 208,
+  //     id_node: "40e9c97e-a1a8-43ad-ac91-7628382b4727",
+  //   },
+  {
+    dataset_name: "sl_gach_nung",
+    dataflow_id: 209,
+    id_node: [
+      "e2181182-0e52-4b77-a3bd-fa14c2211d14",
+      "df655069-8efd-4eb6-9535-650a5ea6ae60",
+      "20793b65-0408-48fc-a970-5ef370e78d64",
+      "d51d56b1-dde5-4b28-85fb-db2fc0519a09",
+      "65a4306d-67ef-447a-b8ce-8919481e71a8",
+      "65a4306d-67ef-447a-b8ce-8919481e71a8",
+    ],
+  },
+  //   {
+  //     dataset_name: "th_nhien_lieu",
+  //     dataflow_id: 209,
+  //     id_node: "df655069-8efd-4eb6-9535-650a5ea6ae60",
+  //   },
+  //   {
+  //     dataset_name: "th_than_chart",
+  //     dataflow_id: 209,
+  //     id_node: "20793b65-0408-48fc-a970-5ef370e78d64",
+  //   },
+  //   {
+  //     dataset_name: "thnhl",
+  //     dataflow_id: 209,
+  //     id_node: "d51d56b1-dde5-4b28-85fb-db2fc0519a09",
+  //   },
+  //   {
+  //     dataset_name: "nhl_chart",
+  //     dataflow_id: 209,
+  //     id_node: "65a4306d-67ef-447a-b8ce-8919481e71a8",
+  //   },
+  //   {
+  //     dataset_name: "thnhl",
+  //     dataflow_id: 209,
+  //     id_node: "65a4306d-67ef-447a-b8ce-8919481e71a8",
+  //   },
+  {
+    dataset_name: "men_sl_giao_lo",
+    dataflow_id: 210,
+    id_node: [
+      "8a05e55a-3804-4718-9f51-973e2a231a83",
+      "1e8188df-0a8b-46e4-8b8d-b6eda8053564",
+      "57cb2da0-897a-4244-a1eb-d4505a3b35cd",
+      "824b0d34-aa84-4860-b5d5-da3b615d8b9b",
+    ],
+  },
+  //   {
+  //     dataset_name: "thmen",
+  //     dataflow_id: 210,
+  //     id_node: "1e8188df-0a8b-46e4-8b8d-b6eda8053564",
+  //   },
+  //   {
+  //     dataset_name: "th_men_E",
+  //     dataflow_id: 210,
+  //     id_node: "57cb2da0-897a-4244-a1eb-d4505a3b35cd",
+  //   },
+  //   {
+  //     dataset_name: "th_men_G",
+  //     dataflow_id: 210,
+  //     id_node: "824b0d34-aa84-4860-b5d5-da3b615d8b9b",
+  //   },
+  {
+    dataset_name: "bcth_mai",
+    dataflow_id: 212,
+    id_node: [
+      "06267a82-c343-4614-997e-d47f0cde17a0",
+      "06267a82-c343-4614-997e-d47f0cde17a0",
+    ],
+  },
+  //   {
+  //     dataset_name: "bcth_th_mai",
+  //     dataflow_id: 212,
+  //     id_node: "06267a82-c343-4614-997e-d47f0cde17a0",
+  //   },
+  {
+    dataset_name: "khsx_final_HNTM",
+    dataflow_id: 215,
+    id_node: ["9cdd8c5d-625b-40e7-9fcf-8bcacdc3ea9e"],
+  },
+  {
+    dataset_name: "tk_loi",
+    dataflow_id: 216,
+    id_node: ["2d3e3ce6-746e-4fe1-8523-23e6293f0857"],
+  },
+  {
+    dataset_name: "bcsx_bcsx1",
+    dataflow_id: 218,
+    id_node: ["7502b601-6277-4220-aa25-f32b1f941ab3"],
+  },
+  {
+    dataset_name: "bcsx_bcsx2_sl",
+    dataflow_id: 219,
+    id_node: [
+      "a7b269b2-4bba-4b0d-825d-9a4ba1be1c68",
+      "530eb8a0-f6cf-4862-8736-6006b61909e7",
+    ],
+  },
+  //   {
+  //     dataset_name: "bcsx_bcsx2_cl",
+  //     dataflow_id: 219,
+  //     id_node: "530eb8a0-f6cf-4862-8736-6006b61909e7",
+  //   },
+  {
+    dataset_name: "lsx",
+    dataflow_id: 221,
+    id_node: [
+      "495da06e-b99a-4126-a68f-f3794526730c",
+      "9f682990-3a48-4716-ae6e-9f5413af202a",
+    ],
+  },
+  //   {
+  //     dataset_name: "	lsx",
+  //     dataflow_id: 221,
+  //     id_node: "9f682990-3a48-4716-ae6e-9f5413af202a",
+  //   },
+  {
+    dataset_name: "lsx_bbnktp",
+    dataflow_id: 223,
+    id_node: ["064a75f2-5919-40c0-84a2-bcab3b22d67e"],
+  },
+  {
+    dataset_name: "lsx_bbnktp_2",
+    dataflow_id: 224,
+    id_node: ["e8673b41-b41d-462e-bff7-d44532377f9f"],
+  },
+  {
+    dataset_name: "hm_dnsx",
+    dataflow_id: 226,
+    id_node: [
+      "4e6e468c-02b6-4fc6-a3e9-eae72cb7d076",
+      "6720804f-7ecf-4b24-8ae9-0cc2ec090a48",
+    ],
+  },
+  //   {
+  //     dataset_name: "dnsx_sum",
+  //     dataflow_id: 226,
+  //     id_node: "6720804f-7ecf-4b24-8ae9-0cc2ec090a48",
+  //   },
+  {
+    dataset_name: "sl_xnt",
+    dataflow_id: 227,
+    id_node: [
+      "d407bf86-99c0-4f02-a7cd-841e9850f22d",
+      "216f4578-2850-463e-b50c-8bad451ee12f",
+      "a09592f4-3c2c-465c-ad37-95d9094c9498",
+      "24bd7b83-3742-43ef-ade4-c65c4631fdb6",
+      "61b92b46-f898-422d-8e58-8ce3067e8230",
+      "d4f91fef-f333-4852-b4ad-23ab25035197",
+      "35a80283-5b3b-44d8-a1fe-d3b8aeb7310c",
+    ],
+  },
+  //   {
+  //     dataset_name: "bk_xk",
+  //     dataflow_id: 227,
+  //     id_node: "216f4578-2850-463e-b50c-8bad451ee12f",
+  //   },
+  //   {
+  //     dataset_name: "bk_tkdk",
+  //     dataflow_id: 227,
+  //     id_node: "a09592f4-3c2c-465c-ad37-95d9094c9498",
+  //   },
+  //   {
+  //     dataset_name: "bk_xnt_ma",
+  //     dataflow_id: 227,
+  //     id_node: "24bd7b83-3742-43ef-ade4-c65c4631fdb6",
+  //   },
+  //   {
+  //     dataset_name: "bk_xnt",
+  //     dataflow_id: 227,
+  //     id_node: "61b92b46-f898-422d-8e58-8ce3067e8230",
+  //   },
+  //   {
+  //     dataset_name: "bk_xnt_mspc2",
+  //     dataflow_id: 227,
+  //     id_node: "d4f91fef-f333-4852-b4ad-23ab25035197",
+  //   },
+  //   {
+  //     dataset_name: "bk_nk",
+  //     dataflow_id: 227,
+  //     id_node: "35a80283-5b3b-44d8-a1fe-d3b8aeb7310c",
+  //   },
+  {
+    dataset_name: "kha_test_refresh_dataset",
+    dataflow_id: 228,
+    id_node: ["591448f4-0d01-48be-8505-26430d9ed513"],
+  },
+  {
+    dataset_name: "ds_bcsx_nksx",
+    dataflow_id: 232,
+    id_node: [
+      "bbe3c54d-737c-4d2a-87c8-5c62b6979dcd",
+      "247e5eb9-b46e-4299-b8aa-59815f19b58c",
+    ],
+  },
+  //   {
+  //     dataset_name: "nksx",
+  //     dataflow_id: 232,
+  //     id_node: "247e5eb9-b46e-4299-b8aa-59815f19b58c",
+  //   },
+  {
+    dataset_name: "kha_test_abc_bk_tkdk",
+    dataflow_id: 236,
+    id_node: ["216f4578-2850-463e-b50c-8bad451ee12f"],
+  },
+  {
+    dataset_name: "bcth_mai_test",
+    dataflow_id: 242,
+    id_node: ["0e1ce5f9-5e9d-4e4c-9960-8088aee33a06"],
+  },
+  {
+    dataset_name: "số liệt tiêu hao mài",
+    dataflow_id: 242,
+    id_node: [
+      "c6d38f67-5659-485f-9870-f6e6ee67f8d9",
+      "c6d38f67-5659-485f-9870-f6e6ee67f8d9",
+    ],
+  },
+  //   {
+  //     dataset_name: "slth_mai",
+  //     dataflow_id: 242,
+  //     id_node: "c6d38f67-5659-485f-9870-f6e6ee67f8d9",
+  //   },
+  {
+    dataset_name: "kha_test_tu_chay_dataset",
+    dataflow_id: 244,
+    id_node: ["6e23f0ad-41d7-4a4b-851a-ca3d30678547"],
+  },
+  {
+    dataset_name: "bbnk_pnk",
+    dataflow_id: 245,
+    id_node: ["971b7cbe-b918-4ee8-ad1a-77a56dbe1ab0"],
+  },
+  {
+    dataset_name: "khsx_ddh-dnsx-lsxdc",
+    dataflow_id: 247,
+    id_node: ["0d5d2710-98ff-4e69-bc32-ea8a56e9d24c"],
+  },
+  {
+    dataset_name: "khsx_ddh_dnsx_lsxdc",
+    dataflow_id: 247,
+    id_node: ["0d5d2710-98ff-4e69-bc32-ea8a56e9d24c"],
+  },
+  {
+    dataset_name: "bk_lsx",
+    dataflow_id: 248,
+    id_node: [
+      "d1668037-7e74-4d7c-b691-eb71481f9960",
+      "637ee945-3571-4577-a882-b68e8fa15d43",
+    ],
+  },
+  //   {
+  //     dataset_name: "lsx_npp",
+  //     dataflow_id: 248,
+  //     id_node: "637ee945-3571-4577-a882-b68e8fa15d43",
+  //   },
+  {
+    dataset_name: "sssx1",
+    dataflow_id: 250,
+    id_node: ["f87a9e57-4d8f-47ec-80a1-76cea805f5a2"],
+  },
+  {
+    dataset_name: "ra_khsx_th_bo",
+    dataflow_id: 251,
+    id_node: [
+      "0b67d3b6-5a84-4879-8b7f-71c35406d6b4",
+      "1d9dd993-36ac-4cf0-8072-0466785a43bb",
+      "de7965bb-6b07-4063-9b73-4846012a7865",
+      "de7965bb-6b07-4063-9b73-4846012a7865",
+    ],
+  },
+  //   {
+  //     dataset_name: "ra_khsx_new",
+  //     dataflow_id: 251,
+  //     id_node: "1d9dd993-36ac-4cf0-8072-0466785a43bb",
+  //   },
+  //   {
+  //     dataset_name: "ra_khsx_th_new",
+  //     dataflow_id: 251,
+  //     id_node: "de7965bb-6b07-4063-9b73-4846012a7865",
+  //   },
+  //   {
+  //     dataset_name: "ra_khsx_th",
+  //     dataflow_id: 251,
+  //     id_node: "de7965bb-6b07-4063-9b73-4846012a7865",
+  //   },
+  //   {
+  //     dataset_name: "bk_xnt_mspc2_new",
+  //     dataflow_id: 252,
+  //     id_node: "d4f91fef-f333-4852-b4ad-23ab25035197",
+  //   },
+  //   {
+  //     dataset_name: "bk_tkdk_new",
+  //     dataflow_id: 252,
+  //     id_node: "a09592f4-3c2c-465c-ad37-95d9094c9498",
+  //   },
+  //   {
+  //     dataset_name: "bk_xk_new",
+  //     dataflow_id: 252,
+  //     id_node: "216f4578-2850-463e-b50c-8bad451ee12f",
+  //   },
+  //   {
+  //     dataset_name: "bk_nk_new",
+  //     dataflow_id: 252,
+  //     id_node: "35a80283-5b3b-44d8-a1fe-d3b8aeb7310c",
+  //   },
+  {
+    dataset_name: "bk_xnt_new",
+    dataflow_id: 252,
+    id_node: [
+      "61b92b46-f898-422d-8e58-8ce3067e8230",
+      "d4f91fef-f333-4852-b4ad-23ab25035197",
+      "a09592f4-3c2c-465c-ad37-95d9094c9498",
+      "216f4578-2850-463e-b50c-8bad451ee12f",
+      "35a80283-5b3b-44d8-a1fe-d3b8aeb7310c",
+    ],
+  },
+  {
+    dataset_name: "round_fix_bug_time",
+    dataflow_id: 253,
+    id_node: ["89fad148-ebb2-4ca4-b00b-33829b3f5469"],
+  },
+  {
+    dataset_name: "tscn_sp",
+    dataflow_id: 254,
+    id_node: ["45205fca-f010-4e00-a1d2-bf0cb507dd9f"],
+  },
+  {
+    dataset_name: "2768_uuid",
+    dataflow_id: 255,
+    id_node: ["f608775e-24e9-4ca6-a16c-f350e086fe22"],
+  },
+  {
+    dataset_name: "sssx1_clone",
+    dataflow_id: 256,
+    id_node: ["d25a460b-cd11-4d2d-b9c8-354507e0ceb5"],
+  },
+  {
+    dataset_name: "hm_dnsx_clone",
+    dataflow_id: 257,
+    id_node: ["e85850a7-dbfd-4d7a-9e82-105de1df20f8"],
+  },
+  {
+    dataset_name: "khsx_clone",
+    dataflow_id: 258,
+    id_node: ["732ed041-e106-45f2-bae6-e86542dac2d8"],
+  },
+  {
+    dataset_name: "api_nk_datatset",
+    dataflow_id: 260,
+    id_node: [
+      "6d421ee7-69f9-4a84-9a75-1d9bc618d17b",
+      "53e75881-5d3f-4d99-8bfb-cab122d31fb9",
+      "43ec88a1-4385-4ec4-8ab8-2eb8107d3316",
+      "04dd7afd-a8d1-4cff-9826-6a6d163c2d39",
+      "1c8a3c67-c2c0-4d38-8fb6-d6c043336f17",
+    ],
+  },
+  //   {
+  //     dataset_name: "api_ddhb_dataset",
+  //     dataflow_id: 260,
+  //     id_node: "53e75881-5d3f-4d99-8bfb-cab122d31fb9",
+  //   },
+  //   {
+  //     dataset_name: "api_hdbh_dataset",
+  //     dataflow_id: 260,
+  //     id_node: "43ec88a1-4385-4ec4-8ab8-2eb8107d3316",
+  //   },
+  //   {
+  //     dataset_name: "api_tkdk_dataset",
+  //     dataflow_id: 260,
+  //     id_node: "04dd7afd-a8d1-4cff-9826-6a6d163c2d39",
+  //   },
+  //   {
+  //     dataset_name: "api_xk_dataset",
+  //     dataflow_id: 260,
+  //     id_node: "1c8a3c67-c2c0-4d38-8fb6-d6c043336f17",
+  //   },
+];
