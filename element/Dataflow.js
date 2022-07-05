9beba3de10d3f0603e59d9815a328ebb29f22179
hanhsymper <hanhdth@symper.vn>
2022-07-05 07:10:26 +0700

    el trong dataflow

---
+++ element/Dataflow.js
@@ -0,0 +1,5 @@
+export const btn_dataflow =
+  ".v-icon.notranslate.collapse.icon-group.v-icon--link.mdi.mdi-view-dashboard-outline.theme--light";
+
+export const btn_list_dataflow =
+  ".v-list-item.v-list-item--dense.v-list-item--link.theme--light";
