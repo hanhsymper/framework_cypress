7bb1daec1d7165f8ecf7e16b3cfbc58b6a0c749a
hanhsymper <hanhdth@symper.vn>
2022-07-05 07:09:45 +0700

    các element trong wfl

---
+++ element/Workflow.mjs
@@ -0,0 +1,4 @@
+export const btn_wfl =
+  ".v-icon.notranslate.collapse.icon-group.v-icon--link.mdi.mdi-lan.theme--light";
+export const btn_list_wfl =
+  ".v-list-item.v-list-item--dense.v-list-item--link.theme--light .v-list-item__title.fm.fs-13";
