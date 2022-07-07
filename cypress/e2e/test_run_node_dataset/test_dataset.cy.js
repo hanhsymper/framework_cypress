c7dcdbd9e3be60cd12ae216742f2d3a80676da4d
hanhsymper <hanhdth@symper.vn>
2022-07-07 13:37:27 +0700

    file test dataset

---
+++ cypress/e2e/test_run_node_dataset/test_dataset.cy.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+// const cypress = require("cypress");
+const { describe } = require("mocha");
+const LoginPage = require("../../../page/LoginPage.js");
+const DataflowPage = require("../../../page/DataFlowPage.mjs");
+const data = require("../../fixtures/data_test_dataflow/data.js");
+
+import * as data_login from "../../../helper/data_login";
+
+export const testNotificationDts = function (data) {
+  describe("test notification", function () {
+    before(() => {
+      // LoginPage.visit();
+      // LoginPage.submit(data_login.EMAIL, data_login.PASSWORD);
+      // DataflowPage.clickWfL();
+      // DataflowPage.findId(data.dataflow_id);
+      // DataflowPage.clickAction("Sửa");
+      cy.loginViaAPISession("dinhnv@symper.vn", "Damthatbai@2010");
+      cy.visit(
+        "https://symper-apps.vthmgroup.vn/#/dataflows/" +
+          data.dataflow_id +
+          "/edit"
+      );
+    });
+    for (let i = 0; i < data.id_node.length; i++) {
+      it(`check display notificatio when update node ${data.id_node[i]}-${data.dataflow_id}`, function () {
+        DataflowPage.clickNode("[model-id=" + data.id_node[i] + "]");
+        DataflowPage.clickRun();
+        DataflowPage.checkDisPlay("Có lỗi xảy ra khi lấy dữ liệu từ API");
+      });
+    }
+  });
+};
+for (let i = 0; i < data.length; i++) {
+  testNotificationDts(data[i]);
+}
