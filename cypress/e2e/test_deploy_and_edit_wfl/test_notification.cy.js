362b0ca807b1afc8bfb4decdfe5c99e18e8a3558
hanhsymper <hanhdth@symper.vn>
2022-07-15 08:11:33 +0700

    cải thiện testscrip thông báo

--- cypress/e2e/test_deploy_and_edit_wfl/test_notification.cy.js
+++ cypress/e2e/test_deploy_and_edit_wfl/test_notification.cy.js
@@ -5,62 +5,44 @@ const LoginPage = require("../../../page/LoginPage.js");
 const WorkflowPage = require("../../../page/WorkflowPage.mjs");
 const data = require("../../fixtures/data_test_noti_wfl/data_id.js");
 
-import * as data_login from "../../../helper/data_login";
+const { handleErrow } = require("../../../helper/handle_ram_overflow_error");
 
 export const testNotificationWfl = function (data) {
   describe("test notification when update", function () {
-    // before(() => {
-    //   cy.loginViaAPISession("dinhnv@symper.vn", "Damthatbai@2010");
-    //   // LoginPage.submit(data_login.EMAIL, data_login.PASSWORD);
-    // });
-    beforeEach(() => {
+    before(() => {
       cy.loginViaAPISession("dinhnv@symper.vn", "Damthatbai@2010");
+      cy.visit("https://stage-apps.symper.vn/#/workflow/");
+      WorkflowPage.clickWfL();
     });
-    it(`check display notificatio when update ${data}`, function () {
-      cy.visit("https://symper-apps.vthmgroup.vn/#/workflow/" + data + "/edit");
 
-      // WorkflowPage.clickWfL();
-      // WorkflowPage.search(data);
-      // WorkflowPage.clickAction("Sửa");
-      WorkflowPage.clickActionSave();
-      WorkflowPage.checkNotification("Update model sucessfully");
-    });
-    it(`check display notificatio when deploy ${data}`, function () {
-      // WorkflowPage.clickWfL();
-      // LoginPage.submit(data_login.EMAIL, data_login.PASSWORD);
-      // WorkflowPage.clickWfL();
-      // WorkflowPage.search(data);
-      cy.visit("https://symper-apps.vthmgroup.vn/#/workflow");
-      WorkflowPage.findId(data);
-      WorkflowPage.clickAction("Deploy");
-      WorkflowPage.checkNotification("Deploy process successfully");
-    });
-    // after(() => {
-    //   cy.clearLocalStorage("symper-login-info");
-    // });
+    for (let i = 0; i < data.length; i++) {
+      beforeEach(() => {
+        cy.loginViaAPISession("dinhnv@symper.vn", "Damthatbai@2010");
+      });
+      it(`check display notificatio when update ${data[i]}`, function () {
+        WorkflowPage.search(data[i]);
+        WorkflowPage.clickAction("Sửa");
+        WorkflowPage.clickActionSave();
+        WorkflowPage.checkNotification("Update model sucessfully");
+        cy.go("back");
+        handleErrow("ttps://stage-apps.symper.vn/#/workflow/");
+        data = data.slice(i);
+      });
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      beforeEach(() => {
+        cy.loginViaAPISession("dinhnv@symper.vn", "Damthatbai@2010");
+      });
+      it(`check display notificatio when deploy ${data[i]}`, function () {
+        // WorkflowPage.clickWfL();
+        WorkflowPage.search(data[i]);
+        WorkflowPage.clickAction("Deploy");
+        WorkflowPage.checkNotification("Deploy process successfully");
+        handleErrow("ttps://stage-apps.symper.vn/#/workflow/");
+        data = data.slice(i);
+      });
+    }
   });
-  // describe("test notification when deploy", function () {
-  //   before(() => {
-  //     LoginPage.visit();
-  //     LoginPage.submit(data_login.EMAIL, data_login.PASSWORD);
-  //     WorkflowPage.clickWfL();
-  //   });
-  //   // it("check display notificatio when update", function () {
-  //   //   WorkflowPage.clickWfL();
-  //   //   WorkflowPage.search(data);
-  //   //   WorkflowPage.clickAction("Sửa");
-  //   //   cy.wait(20000);
-  //   //   WorkflowPage.clickActionSave();
-  //   //   WorkflowPage.checkNotification("Update model sucessfully");
-  //   //   cy.go("back");
-  //   // });
-  //   it("check display notificatio when deploy", function () {
-  //     WorkflowPage.search(data);
-  //     WorkflowPage.clickAction("Deploy");
-  //     WorkflowPage.checkNotification("Deploy process successfully");
-  //   });
-  // });
 };
-for (let i = 0; i < data.length; i++) {
-  testNotificationWfl(data[i]);
-}
+testNotificationWfl(data);
